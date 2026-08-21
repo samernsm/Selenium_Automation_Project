841956d0f0162c5778572d25940d2b399169d36d
Samer Nabil <samer_nsm@yahoo.com.com>
2026-08-21 15:02:09 +0300

    Fix Firefox driver support

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,21 +1,30 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 from config.settings import BROWSER, HEADLESS
 
 
 def create_driver():
-    if BROWSER != "chrome":
-        raise ValueError(
-            f"Unsupported browser: {BROWSER}. "
-            "Currently supported: chrome."
-        )
+    if BROWSER == "chrome":
+        options = ChromeOptions()
 
-    options = Options()
+        if HEADLESS:
+            options.add_argument("--headless=new")
 
-    if HEADLESS:
-        options.add_argument("--headless=new")
         options.add_argument("--window-size=1920,1080")
 
-    return webdriver.Chrome(options=options)
+        return webdriver.Chrome(options=options)
 
+    if BROWSER == "firefox":
+        options = FirefoxOptions()
+
+        if HEADLESS:
+            options.add_argument("-headless")
+
+        return webdriver.Firefox(options=options)
+
+    raise ValueError(
+        f"Unsupported browser: {BROWSER}. "
+        "Supported browsers: chrome, firefox."
+    )
